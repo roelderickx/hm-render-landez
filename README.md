ac4de5a49e6a516d45876de0ea4263683ccb6cd7
roelderickx <roel.derickx@gmail.com>
2019-10-19 21:56:45 +0200

    Add installer

--- README.md
+++ README.md
@@ -1,102 +1,10 @@
-# Rendering paper maps using tiles
+# Mapnik rendering
 
-This script renders an area with given boundaries using tiles from either a [mapbox MBTiles file](https://wiki.openstreetmap.org/wiki/MBTiles), a [tile server](https://wiki.openstreetmap.org/wiki/Tile_servers) or locally rendered using [mapnik](http://mapnik.org/). It is designed to work with hikingmap but it can be used standalone as well if desired.
+This program renders an area with given boundaries using landez and mapnik. It is designed to work with hikingmap but it can be used standalone as well if desired.
 
-## Usage
-
-`render.py [OPTION]... gpxfiles...`
-
-Options:
-
-| Parameter | Description
-| --------- | -----------
-| `-o` | Minimum longitude of the area boundaries
-| `-O` | Maximum longitude of the area boundaries
-| `-a` | Minimum latitude of the area boundaries
-| `-A` | Maximum latitude of the area boundaries
-| `-w` | Page width in cm
-| `-h` | Page height in cm
-| `-t` | Temp track file to render. This is used to draw the page boundaries of the overview map, hikingmap will save those as a temporary GPX file.
-| `-y` | Temp waypoints file to render. This is used to render the distance each kilometer or mile, hikingmap will save those waypoints as a temporary GPX file.
-| `-v, --verbose` | Display extra information while processing.
-| `-h, --help` | Display help
-| `gpxfiles` | The GPX track(s) to render.
-
-## Prerequisites
-
-To run this script you should have a working installation of [python 3](https://www.python.org/), [Landez](https://github.com/makinacorpus/landez) and [mapnik](http://mapnik.org/). Make sure you also have [python-mapnik](https://github.com/mapnik/python-mapnik/) installed.
-
-## Tile sources
-
-The tiles are cached in a local folder using the tile manager of Landez. Landez will add tiles to the cache when necessary, supporting a number of possible sources.
-
-### MBTiles
-
-This is a file format from mapbox, they can be created using tools such as (but not limited to) [mapbox mbutil](https://github.com/mapbox/mbutil) or [Maperitive](http://maperitive.net/). Please note that only raster MBTiles files are supported, vector MBTiles files require rendering before use.
-
-### Tile server
-
-This is the easiest to set up, Landez will simply download tiles from a web source. Make sure you don't violate [usage policies](https://operations.osmfoundation.org/policies/tiles/) if you render too many maps at once or set the dpi value too high.
-
-### Mapnik
-
-Since mapnik is installed as a prerequisite of this project, it is included as a possible tile source. However the setup is rather difficult. Consult the documentation of [render_mapnik](https://github.com/roelderickx/hikingmap/blob/master/documentation/render_mapnik.html) in the hikingmap package to get an idea.
-
-### WMS server
-
-Although Landez supports WMS sources, it is not configurable here. WMS servers are generally replaced by WMTS servers, which use fewer resources on the server side.
-
-## Configuration
-
-Apart from these parameters there are some specific parameters for this renderer. They need to be configured in the file render_tiles.config.xml. An example is included in this repository:
-
+## Installation
+Clone this repository and run the following command in the created directory.
+```bash
+python setup.py install
 ```
-<?xml version="1.0" encoding="utf-8"?>
-<render_tiles>
-    <tilesmanager>
-        <mbtiles_file>mbsource.mbtiles</mbtiles_file>
-        <mapnik_stylefile>mapnik_style.xml</mapnik_stylefile>
-        <wmts_url>https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png</wmts_url>
-        <wmts_subdomains>abc</wmts_subdomains>
-        <cache_dir>tilecache</cache_dir>
-        <tile_size>256</tile_size>
-        <tile_format>image/png</tile_format>
-        <tile_scheme>wmts</tile_scheme>
-    </tilesmanager>
-    <hikingmapstyle>hikingmap_style.xml</hikingmapstyle>
-    <outputformat>png</outputformat>
-    <dpi>300</dpi>
-    <scalefactor>1.0</scalefactor>
-    <fontdirs>
-        <fontdir>/usr/share/fonts/noto</fontdir>
-        <fontdir>/usr/share/fonts/noto-cjk</fontdir>
-        <fontdir>/usr/share/fonts/TTF</fontdir>
-    </fontdirs>
-</render_tiles>
-```
-
-Options:
-
-| Tag | Description
-| --- | -----------
-| mbtiles_file | Optional. An MBTiles file providing raster tiles.
-| mapnik_stylefile | Optional. Mapnik stylesheet file.
-| wmts_url | Optional. Remote URL to download tiles.
-| wmts_subdomains | Optional. URL subdomains.
-| cache_dir | Local folder containing cached tiles.
-| tile_size | Tile size.
-| tile_format | Tile image format.
-| tile_scheme | Tile scheme, value can be tms or wmts.
-| hikingmapstyle | The filename of the hikingmap stylesheet. This stylesheet contains the styles to draw the GPX track and waypoints.
-| outputformat | Output format. See the [mapnik documentation](http://mapnik.org/docs/v2.2.0/api/python/mapnik._mapnik-module.html#render_to_file) for possible values.
-| dpi | Amount of detail to render in dots per inch. This value is unrelated to the setting on your printer, a higher value will simply result in smaller icons, thinner roads and unreadable text.
-| scalefactor | The scale factor to use when rendering to image formats.
-| fontdirs | Optional. Can contain one or more fontdir subtags with additional font directories to be used by mapnik.
-
-### Tile source precedence
-
-Please note you should at least add one tile source. Although it is possible to define all tile sources together in &lt;tilesmanager&gt;, Landez will use only one of them. The precedence is:
-1. MBTiles. Fill the tag &lt;mbtiles_file&gt; if you want to use this.
-2. Mapnik. Fill the tag &lt;mapnik\_stylefile&gt; if you want to use this, but leave &lt;mbtiles\_file&gt; blank or remove this tag.
-3. Tile server. Fill the tags &lt;wmts\_url&gt; and &lt;wmts_subdomains&gt; if you want to use this, but leave &lt;mbtiles\_file&gt; and &lt;mapnik\_stylefile&gt; blank or remove them.
 
---
+++ hm_render_landez/hm_render_landez.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python3
+
+# hm-render-mapnik -- render maps on paper using tiles with landez and mapnik
+# Copyright (C) 2019  Roel Derickx <roel.derickx AT gmail>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse, math, mapnik
+from xml.dom import minidom
+from landez import TilesManager, GoogleProjection, ExtractionError
+
+# global constants
+earthCircumference = 40041.44 # km (average, equatorial 40075.017 km / meridional 40007.86 km)
+cmToKmFactor = 100000.0
+inch = 2.54 # cm
+
+def parse_commandline():
+    parser = argparse.ArgumentParser(description = "Render a map on paper using mapnik")
+    parser.add_argument('--pagewidth', dest = 'pagewidth', type = float, default = 20.0, \
+                        help = "page width in cm")
+    parser.add_argument('--pageheight', dest = 'pageheight', type = float, default = 28.7, \
+                        help = "page height in cm")
+    parser.add_argument('-b', '--basename', dest = 'basefilename', default = "detail", \
+                        help = "base filename without extension")
+    parser.add_argument('-t', dest = 'temptrackfile', \
+                        help = "temp track file to render")
+    parser.add_argument('-y', dest = 'tempwaypointfile', \
+                        help = "temp waypoints file to render")
+    parser.add_argument('-v', dest = 'verbose', action = 'store_true')
+    # hm-render-landez specific parameters
+    parser.add_argument('-d', '--dpi', type=int, default=300, \
+                        help = "amount of detail to render in dots per inch (default: %(default)s)")
+    parser.add_argument('-S', '--scale-factor', type=float, default=1.0, \
+                        help = "scale factor (default: %(default)s)")
+    parser.add_argument('--hikingmapstyle', default='hikingmap_style.xml', \
+                        help = "hikingmap stylesheet file, contains the CartoCSS for " + \
+                               "the tracks and the waypoints (default: %(default)s)")
+    parser.add_argument('-f', '--format', dest='output_format', default='png', \
+                        help = "output format, consult the mapnik documentation for " + \
+                               "possible values (default: %(default)s)")
+    parser.add_argument('--mbtiles', dest='mbtiles_file', \
+                        help = "input raster mbtiles file")
+    parser.add_argument('--mapnik-style', dest='mapnik_stylefile', \
+                        help = "mapnik stylesheet file")
+    parser.add_argument('--wmts-url', dest='wmts_url', \
+                        help = "remote URL to download tiles")
+    parser.add_argument('--wmts-subdomains', dest='wmts_subdomains', \
+                        help = "URL subdomains")
+    parser.add_argument('--cachedir', dest='cache_dir', \
+                        help = "local folder containing cached tiles")
+    parser.add_argument('--tilesize', type=int, default=256, dest='tile_size', \
+                        help = "tile size")
+    parser.add_argument('--tileformat', default='image/png', dest='tile_format', \
+                        help = "tile image format")
+    parser.add_argument('--tilescheme', choices=[ 'tms', 'wmts' ], default='wmts', \
+                        dest='tile_scheme', help = "tile scheme")
+    # --
+    parser.add_argument('gpxfiles', nargs = '*')
+    
+    subparsers = parser.add_subparsers(dest='mode', help='bounding box or center mode')
+    
+    # create the parser for the bbox command
+    parser_bbox = subparsers.add_parser('bbox', help='define bounding box')
+    parser_bbox.add_argument('-o', '--minlon', type=float, required = True, \
+                        help = "minimum longitude")
+    parser_bbox.add_argument('-O', '--maxlon', type=float, required = True, \
+                        help = "maximum longitude")
+    parser_bbox.add_argument('-a', '--minlat', type=float, required = True, \
+                        help = "minimum latitude")
+    parser_bbox.add_argument('-A', '--maxlat', type=float, required = True, \
+                        help = "maximum latitude")
+
+    # create the parser for the atlas command
+    parser_atlas = subparsers.add_parser('center', help='define center mode')
+    parser_atlas.add_argument('--lon', type=float, required=True, \
+                              help='longitude of the center of map')
+    parser_atlas.add_argument('--lat', type=float, required=True, \
+                              help='latitude of the center of map')
+    parser_atlas.add_argument('--scale', type=int, default=50000, \
+                              help='scale denominator')
+
+    return parser.parse_args()
+
+
+def convert_cm_to_degrees_lon(lengthcm, scale, latitude):
+    lengthkm = lengthcm / cmToKmFactor * scale
+    return lengthkm / ((earthCircumference / 360.0) * math.cos(math.radians(latitude)))
+
+
+def convert_cm_to_degrees_lat(lengthcm, scale):
+    lengthkm = lengthcm / cmToKmFactor * scale
+    return lengthkm / (earthCircumference / 360.0)
+
+
+def assure_bbox_mode(parameters):
+    if parameters.mode == 'center':
+        pagesize_lon = convert_cm_to_degrees_lon(parameters.pagewidth, \
+                                                 parameters.scale, parameters.lat)
+        pagesize_lat = convert_cm_to_degrees_lat(parameters.pageheight, parameters.scale)
+        
+        parameters.minlon = parameters.lon - pagesize_lon / 2
+        parameters.minlat = parameters.lat - pagesize_lat / 2
+        parameters.maxlon = parameters.lon + pagesize_lon / 2
+        parameters.maxlat = parameters.lat + pagesize_lat / 2
+
+'''
+def __get_xml_subtag_value(self, xmlnode, sublabelname, defaultvalue):
+    elements = xmlnode.getElementsByTagName(sublabelname)
+    return str(elements[0].firstChild.nodeValue) \
+                  if elements and elements[0].childNodes \
+                  else defaultvalue
+
+
+def parse_configfile(self):
+    xmldoc = minidom.parse("render_tiles.config.xml")
+    xmlmapnik = xmldoc.getElementsByTagName('render_tiles')[0]
+    
+    xmltilesmanager = xmlmapnik.getElementsByTagName('tilesmanager')[0]
+    self.mbtiles_file = self.__get_xml_subtag_value(xmltilesmanager, 'mbtiles_file', '')
+    self.wmts_url = self.__get_xml_subtag_value(xmltilesmanager, 'wmts_url', '')
+    self.wmts_subdomains = self.__get_xml_subtag_value(xmltilesmanager, 'wmts_subdomains', '')
+    self.mapnik_stylefile = self.__get_xml_subtag_value(xmltilesmanager, 'mapnik_stylefile', '')
+    self.cache_dir = self.__get_xml_subtag_value(xmltilesmanager, 'cache_dir', '')
+    self.tile_size = int(self.__get_xml_subtag_value(xmltilesmanager, 'tile_size', ''))
+    self.tile_format = self.__get_xml_subtag_value(xmltilesmanager, 'tile_format', '')
+    self.tile_scheme = self.__get_xml_subtag_value(xmltilesmanager, 'tile_scheme', '')
+
+    self.hikingmapstyle = self.__get_xml_subtag_value(xmlmapnik, 'hikingmapstyle', \
+                                                      'hikingmap_style.xml')
+    self.output_format = self.__get_xml_subtag_value(xmlmapnik, 'outputformat', 'pdf')
+    self.dpi = int(self.__get_xml_subtag_value(xmlmapnik, 'dpi', '300'))
+    self.scale_factor = float(self.__get_xml_subtag_value(xmlmapnik, 'scalefactor', '1.0'))
+    
+    xmlfontdirlist = xmlmapnik.getElementsByTagName('fontdirs')
+    
+    for xmlfontdir in xmlfontdirlist:
+        fontdir = self.__get_xml_subtag_value(xmlfontdir, 'fontdir', '')
+        if fontdir != '':
+            mapnik.FontEngine.register_fonts(fontdir, True)
+    
+    return True
+'''
+    
+def create_tiles_manager(parameters):
+    tm_args = { }
+    tm_args['cache'] = True
+    if parameters.mbtiles_file:
+        tm_args['mbtiles_file'] = parameters.mbtiles_file
+    if parameters.wmts_url:
+        tm_args['tiles_url'] = parameters.wmts_url
+        tm_args['tiles_headers'] = { }
+        tm_args['tiles_headers']['User-Agent'] = 'hikingmap/render_tiles'
+        tm_args['tiles_headers']['Accept'] = 'image/png,image/*;q=0.9,*/*;q=0.8'
+    if parameters.wmts_subdomains:
+        tm_args['tiles_subdomains'] = parameters.wmts_subdomains
+    if parameters.mapnik_stylefile:
+        tm_args['stylefile'] = parameters.mapnik_stylefile
+    if parameters.cache_dir:
+        tm_args['tiles_dir'] = parameters.cache_dir
+    if parameters.tile_size:
+        tm_args['tile_size'] = parameters.tile_size
+    if parameters.tile_format:
+        tm_args['tile_format'] = parameters.tile_format
+    if parameters.tile_scheme:
+        tm_args['tile_scheme'] = parameters.tile_scheme
+    
+    tm = TilesManager(**tm_args)
+    
+    return tm
+
+
+def latRad(lat):
+    sin = math.sin(math.radians(lat))
+    radX2 = math.log((1 + sin) / (1 - sin)) / 2
+    return max(min(radX2, math.pi), -math.pi) / 2
+
+
+def zoom(mapPx, worldPx, fraction):
+    return math.floor(math.log(mapPx / worldPx / fraction, 2))
+
+
+def calc_zoomlevel(pagewidth, pageheight, dpi, scalefactor, tilesize, \
+                   minlon, minlat, maxlon, maxlat):
+    px_width = (pagewidth * dpi / inch) / scalefactor
+    px_height = (pageheight * dpi / inch) / scalefactor
+    
+    lat_fraction = (latRad(maxlat) - latRad(minlat)) / math.pi
+    lon_fraction = maxlon - minlon
+    if lon_fraction < 0:
+        lon_fraction += 360
+    lon_fraction /= 360
+    
+    lat_zoom = zoom(px_height, tilesize, lat_fraction)
+    lon_zoom = zoom(px_width, tilesize, lon_fraction)
+
+    return [ min(lat_zoom, lon_zoom) ]
+
+
+def render(parameters):
+    if not parameters.verbose:
+        mapnik.logger.set_severity(getattr(mapnik.severity_type, 'None'))
+
+    merc = mapnik.Projection('+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over')
+    longlat = mapnik.Projection('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+
+    imgwidth = math.trunc(parameters.pagewidth / inch * parameters.dpi)
+    imgheight = math.trunc(parameters.pageheight / inch * parameters.dpi)
+
+    m = mapnik.Map(imgwidth, imgheight)
+    #mapnik.load_map(m, parameters.mapstyle)
+    mapnik.load_map(m, parameters.hikingmapstyle)
+    m.srs = merc.params()
+
+    if hasattr(mapnik, 'Box2d'):
+        bbox = mapnik.Box2d(parameters.minlon, parameters.minlat, parameters.maxlon, parameters.maxlat)
+    else:
+        bbox = mapnik.Envelope(parameters.minlon, parameters.minlat, parameters.maxlon, parameters.maxlat)
+
+    transform = mapnik.ProjTransform(longlat, merc)
+    merc_bbox = transform.forward(bbox)
+    m.zoom_to_box(merc_bbox)
+
+    # create raster symbolizer / rule / style
+    rastersymbolizer = mapnik.RasterSymbolizer()
+    rastersymbolizer.opacity = 1.0
+    rasterrule = mapnik.Rule()
+    rasterrule.symbols.append(rastersymbolizer)
+    rasterstyle = mapnik.Style()
+    rasterstyle.rules.append(rasterrule)
+    m.append_style('RasterStyle', rasterstyle)
+
+    # fetch tiles using the landez TileManager
+    tm = create_tiles_manager(parameters)
+    zoomlevel = calc_zoomlevel(parameters.pagewidth, parameters.pageheight, parameters.dpi, \
+                               parameters.scale_factor, parameters.tile_size, \
+                               parameters.minlon, parameters.minlat, \
+                               parameters.maxlon, parameters.maxlat)
+    if parameters.verbose:
+        print("Calculated zoomlevel = %d" % zoomlevel[0])
+
+    for tile in tm.tileslist(bbox = (parameters.minlon, parameters.minlat, \
+                                     parameters.maxlon, parameters.maxlat), \
+                             zoomlevels = zoomlevel):
+        # calc tile metadata
+        tile_path = tm.cache.tile_fullpath(tile)
+        proj = GoogleProjection(tm.tile_size, zoomlevel, tm.tile_scheme)
+        (tile_lox, tile_loy, tile_hix, tile_hiy) = proj.tile_bbox(tile)
+        try:
+            # make sure the tile is on disk; tiledata is not needed
+            tiledata = tm.tile(tile)
+            # add to mapnik layer
+            rasterlayer = mapnik.Layer("RasterLayer-%d-%d-%d" % tile)
+            rasterlayer.datasource = mapnik.Raster(file = tile_path, \
+                                                   lox = tile_lox, loy = tile_loy, \
+                                                   hix = tile_hix, hiy = tile_hiy)
+            rasterlayer.styles.append('RasterStyle')
+            m.layers.append(rasterlayer)
+        except ExtractionError:
+            print("warning: missing tile zoom=%d x=%d y=%d" % tile)
+
+    for gpxfile in parameters.gpxfiles:
+        gpxlayer = mapnik.Layer('GPXLayer')
+        gpxlayer.datasource = mapnik.Ogr(file = gpxfile, layer = 'tracks')
+        gpxlayer.styles.append('GPXStyle')
+        m.layers.append(gpxlayer)
+
+    if parameters.temptrackfile:
+        overviewlayer = mapnik.Layer('OverviewLayer')
+        overviewlayer.datasource = mapnik.Ogr(file = parameters.temptrackfile, layer = 'tracks')
+        overviewlayer.styles.append('GPXStyle')
+        m.layers.append(overviewlayer)
+    
+    if parameters.tempwaypointfile:
+        waypointlayer = mapnik.Layer('WaypointLayer')
+        waypointlayer.datasource = mapnik.Ogr(file = parameters.tempwaypointfile, layer = 'waypoints')
+        waypointlayer.styles.append('WaypointStyle')
+        m.layers.append(waypointlayer)
+
+    mapnik.render_to_file(m, parameters.basefilename + "." + parameters.output_format,
+                          parameters.output_format,
+                          parameters.scale_factor)
+
+
+def main():
+    parameters = parse_commandline()
+    assure_bbox_mode(parameters)
+
+    #parse_configfile(parameters)
+    
+    render(parameters)
+
+
+if __name__ == '__main__':
+    main()
+
